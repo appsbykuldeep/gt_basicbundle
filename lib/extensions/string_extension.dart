267d4f84f085935865f9ddc25950ea755cdf6d31
appsbykuldeep <kuldeepgupta983@gmail.com>
2022-09-22 18:20:24 +0530

    commit by kd

--- lib/extensions/string_extension.dart
+++ lib/extensions/string_extension.dart
@@ -133,6 +133,19 @@ extension KdStringExt on String {
   //   return url;
   // }
 
+  String get countryFlagEmojicountryCode {
+    final String currencyFlag = this;
+    // 0x41 is Letter A
+    // 0x1F1E6 is Regional Indicator Symbol Letter A
+    // Example :
+    // firstLetter U => 20 + 0x1F1E6
+    // secondLetter S => 18 + 0x1F1E6
+    // See: https://en.wikipedia.org/wiki/Regional_Indicator_Symbol
+    final int firstLetter = currencyFlag.codeUnitAt(0) - 0x41 + 0x1F1E6;
+    final int secondLetter = currencyFlag.codeUnitAt(1) - 0x41 + 0x1F1E6;
+    return String.fromCharCode(firstLetter) + String.fromCharCode(secondLetter);
+  }
+
   String get gtProperCase {
     return replaceAll(RegExp(' +'), ' ')
         .split(" ")
